9452207a1238d69d9c2ab23f47f3376b883e2d1b
Mudit Nawalgaria <mudit.nawalgaria@gmail.com>
2021-12-22 11:29:26 +0530

    Sell properties

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,14 +4,30 @@ const dbConnection = require("../utils/dbConnection");
 
 // Home Page
 exports.homePage = async (req, res, next) => {
-    const [row] = await dbConnection.execute("SELECT * FROM `users` WHERE `email`=?", [req.session.userID]);
-
+    const [row] = await dbConnection.execute("SELECT * FROM `remsusers` WHERE `email`=?", [req.session.userID]);
+    const [rowdetails] = await dbConnection.execute("SELECT * FROM `remsdetails` WHERE `phone`=?", [row[0].phone])
     if (row.length !== 1) {
         return res.redirect('/logout');
     }
 
     res.render('home', {
-        user: row[0]
+        user: row[0],
+        userdetails: rowdetails[0]
+    });
+}
+
+exports.sellPage = async (req, res, next) => {
+    const [row] = await dbConnection.execute("SELECT * FROM `remsusers` WHERE `email`=?", [req.session.userID]);
+    console.log(row[0].phone);
+    const [rowdetails] = await dbConnection.execute("SELECT * FROM `remsdetails` WHERE `phone`=?", [row[0].phone])
+    // console.log(rowdetails[0]);    
+    if (row.length !== 1) {
+        return res.redirect('/logout');
+    }
+
+    res.render('sell', {
+        user: row[0],
+        userdetails: rowdetails[0]
     });
 }
 
@@ -34,7 +50,7 @@ exports.register = async (req, res, next) => {
     try {
 
         const [row] = await dbConnection.execute(
-            "SELECT * FROM `users` WHERE `email`=?",
+            "SELECT * FROM `remsusers` WHERE `email`=?",
             [body._email]
         );
 
@@ -47,11 +63,18 @@ exports.register = async (req, res, next) => {
         const hashPass = await bcrypt.hash(body._password, 12);
 
         const [rows] = await dbConnection.execute(
-            "INSERT INTO `users`(`name`,`email`,`phone`,`address`,`password`) VALUES(?,?,?,?,?)",
-            [body._name, body._email,body._phone,body._address, hashPass]
+            "INSERT INTO `remsusers`(`email`,`password`,`phone`) VALUES(?,?,?)",
+            [body._email,hashPass,body._phone]
         );
-
-        if (rows.affectedRows !== 1) {
+        const [rowsdetails] = await dbConnection.execute(
+            "INSERT INTO `remsdetails`(`phone`,`name`,`address`) VALUES(?,?,?)",
+            [body._phone,body._name,body._address]
+        );
+        // const [rows] = await dbConnection.execute(
+        //     "INSERT INTO `remsusers`(`name`,`email`,`phone`,`address`,`password`) VALUES(?,?,?,?,?)",
+        //     [body._name, body._email,body._phone,body._address, hashPass]
+        // );
+        if (rows.affectedRows !== 1 || rowsdetails.affectedRows !== 1) {
             return res.render('register', {
                 error: 'Your registration has failed.'
             });
@@ -71,6 +94,43 @@ exports.loginPage = (req, res, next) => {
     res.render("login");
 };
 
+exports.addProperty = async (req,res,next) => {
+    const errors = validationResult(req);
+    const {body} = req;
+
+    if (!errors.isEmpty()) {
+        return res.render('sell',{
+            error: errors.array()[0].msg
+        })
+    }
+
+    try{
+        var option;
+        if (body.rent == "Sell"){
+            option = 0
+        }
+        else if (body.rent == "Rent"){
+            option = 1
+        }
+        const [rows] = await dbConnection.execute(
+            "INSERT INTO `properties`(`userid`,`address`,`city`,`state`,`zip`,`description`,`price`,`area`,`rent`) VALUES(?,?,?,?,?,?,?,?,?)",
+            [req.session.userID,body.inputAddress,body.inputCity,body.inputState,body.inputZip,body.description,body.price,body.area,option]
+        );
+        if (rows.affectedRows !== 1) {
+            return res.render('sell', {
+                error: 'Property could not be added. Try again.'
+            });
+        }
+        res.render("sell", {
+            msg: 'You have successfully added property.'
+        });
+    }
+    catch(e){
+        next(e);
+    }
+
+}
+
 // Login User
 exports.login = async (req, res, next) => {
 
@@ -85,7 +145,7 @@ exports.login = async (req, res, next) => {
 
     try {
 
-        const [row] = await dbConnection.execute('SELECT * FROM `users` WHERE `email`=?', [body._email]);
+        const [row] = await dbConnection.execute('SELECT * FROM `remsusers` WHERE `email`=?', [body._email]);
 
         if (row.length != 1) {
             return res.render('login', {
--- routes.js
+++ routes.js
@@ -3,10 +3,12 @@ const { body } = require("express-validator");
 
 const {
     homePage,
+    sellPage,
     register,
     registerPage,
     login,
     loginPage,
+    addProperty,
 } = require("./controllers/userController");
 
 const ifNotLoggedin = (req, res, next) => {
@@ -26,6 +28,7 @@ const ifLoggedin = (req,res,next) => {
 router.get('/', ifNotLoggedin, homePage);
 
 router.get("/login", ifLoggedin, loginPage);
+
 router.post("/login",
 ifLoggedin,
     [
@@ -41,7 +44,34 @@ ifLoggedin,
     ],
     login
 );
-
+router.get("/sellPage",ifNotLoggedin, sellPage);
+router.post("/sellPage",
+    ifNotLoggedin,
+    [
+        body("inputAddress","Address must not be empty")
+            .notEmpty()
+            .trim(),
+        body("inputState","State must not be empty")
+            .notEmpty()
+            .trim(),
+        body("inputZip","Enter 6 digit zip code")
+            .notEmpty()
+            .trim()
+            .isLength({min:6,max:6}),
+        body("inputCity","City must not be empty")
+            .notEmpty()
+            .trim(),        
+        body("description","Enter a minimum of 20 characters in description for better reference")
+            .notEmpty()
+            .trim()
+            .isLength({min:20}),
+        body("area","Area must not be empty")
+            .notEmpty(),
+        body("price","Price must not be empty")
+            .notEmpty(),
+    ],
+    addProperty
+)
 router.get("/signup", ifLoggedin, registerPage);
 router.post(
     "/signup",
